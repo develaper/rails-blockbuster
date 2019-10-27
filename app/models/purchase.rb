5632dce3277027fc0cb3d44ddc5635b3f979280d
IbrahXceed <ibrahim@xceed.me>
2019-10-27 16:31:30 +0100

    Adds callback to validate uniqueness of the Purchase before its creation

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,4 +6,10 @@ class Purchase < ApplicationRecord
   belongs_to :user
 
   validates_presence_of [:price, :quality, :status]
+
+  before_create :validate_purchase_uniqueness 
+
+  def validate_purchase_uniqueness
+    !Purchase.alive.where(:user => user, :content => content).any?
+  end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -9,4 +9,28 @@ RSpec.describe Purchase, type: :model do
       it { should validate_presence_of(:status) }
     end
   end
+  describe 'validate uniqueness to avoid duplication' do
+    before(:all) do
+      @user = FactoryBot.create(:user)
+      @content = FactoryBot.create(:content)
+      @purchase_alive = FactoryBot.build(:purchase, status: 'alive', content: @content, user: @user)
+    end
+    context 'there is non existing previous purchase for the content' do
+      it 'passes successfully' do
+        expect(@purchase_alive.validate_purchase_uniqueness).to be_truthy
+      end
+    end
+    context 'there is a previous purchase for the content already expired' do
+      it 'passes successfully' do
+        purchase_expìred = FactoryBot.create(:purchase, status: 'expired', content: @content, user: @user)
+        expect(@purchase_alive.validate_purchase_uniqueness).to be_truthy
+      end
+    end
+    context 'there is a previous purchase alive for the content' do
+      it 'passes successfully' do
+        previous_purchase_alive = FactoryBot.create(:purchase, status: 'alive', content: @content, user: @user)
+        expect(@purchase_alive.validate_purchase_uniqueness).to be false
+      end
+    end
+  end
 end
