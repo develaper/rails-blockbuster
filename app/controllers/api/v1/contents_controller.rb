ca939e240ddc44e12e24b151a1a88b332300b8ec
IbrahXceed <ibrahim@xceed.me>
2019-10-29 19:44:59 +0100

    Changes ContentsController to remove duplicated code on rendering

--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -2,20 +2,12 @@ class Api::V1::ContentsController < ApplicationController
   def index
     case params[:category]
     when "season"
-      @contents = Content.includes(:episodes).all.where(category:params[:category]).order(:created_at)
-      respond_to do |format|
-        format.json { render json: @contents.as_json(include: :episodes) }
-      end
+      @contents = Content.includes(:episodes).all.where(category:params[:category]).order(:created_at).as_json(include: :episodes)
     when "movie"
       @contents = Content.all.where(category:params[:category]).order(:created_at)
-      respond_to do |format|
-        format.json { render json: @contents }
-      end
     else
       @contents = Content.all.order(:created_at)
-      respond_to do |format|
-        format.json { render json: @contents }
-      end
     end
+    render json: @contents
   end
 end
